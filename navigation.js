c8d4a974f39bbdbdde250e1243b65ea6715fe0e3
Kai Liu (Victor) <vic.kai.liu@gmail.com>
2015-06-10 09:50:17 +0800

    remove submission date

--- navigation.js
+++ navigation.js
@@ -3,22 +3,22 @@
   // document.getElementById("sites-chrome-sidebar-left").innerHTML='<object type="text/html" data="navigation.html" ></object>';
    // your page initialization code here
    // the DOM will be available here
-  var due = new Date();
-  due.setFullYear(2015, 5, 8);
+  //var due = new Date();
+  //due.setFullYear(2015, 5, 8);
   var event_date = new Date();
   event_date.setFullYear(2015, 11, 13);
   var date = new Date();
 
   var days = 1000 * 60 * 60 * 24;
-  var dueDays_paper = Math.floor((due.getTime()-date.getTime())/days);
+  //var dueDays_paper = Math.floor((due.getTime()-date.getTime())/days);
   var dueDays_event = Math.floor((event_date.getTime()-date.getTime())/days);
 
-  if (dueDays_paper >=0) {
-    document.getElementById("countdown_paper_dig").innerHTML = dueDays_paper;
-    if (dueDays_paper <=1) {
-      document.getElementById("countdown_paper_text").innerHTML = "day until";
-    }
-  }
+  // if (dueDays_paper >=0) {
+  //   document.getElementById("countdown_paper_dig").innerHTML = dueDays_paper;
+  //   if (dueDays_paper <=1) {
+  //     document.getElementById("countdown_paper_text").innerHTML = "day until";
+  //   }
+  // }
 
   if (dueDays_event >=0) {
     document.getElementById("countdown_event_dig").innerHTML = dueDays_event;
